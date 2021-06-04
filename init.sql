21fcab3438dca6158d70a155dca6ba3ec82ff1df
Victor Näslund <victor.naslund@greyhash.se>
2021-06-04 12:48:43 +0200

    Added example autostart script

---
+++ init.sql
@@ -0,0 +1,13 @@
+use dfgdg;
+
+CREATE TABLE IF NOT EXISTS Customers
+(
+    id INT NOT NULL,
+    employee_id INT NOT NULL,
+    customer_id INT NOT NULL,
+    call_outcome_id INT NOT NULL
+);
+
+INSERT INTO Customers VALUES (1,2,3,4);
+
+
